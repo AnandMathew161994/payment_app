8b44355dd6c192fc1e62efcd9616ba873333b969
Anand <studywithaby16@gmail.com>
2024-09-07 15:58:54 -0400

    landing

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,16 @@
 import { useLocation } from "react-router";
 import { Appbar } from "../components/Appbar"
+import { Warningbox } from "../components/Warningbox";
 
 export const Dashboard = () => {
     const location = useLocation();
     return <div>
-        {console.log(location.state.firstname)}
-        <Appbar firstname={location.state.firstname} lastname={location.state.lastname} balance = {location.state.balance}/>
+       {location.state ? (
+      <Appbar firstname={location.state.firstname} lastname={location.state.lastname} balance = {location.state.balance}/>
+    ) :  (<>
+    <>You are not Signed-in</>
+    <Warningbox label= {"Please"}  buttonText={"Sign-in"} to={"/signin"} />
+    </>) }
         </div>
     
 }
